acc459e119f51732b4f82f4d34d96bb8b46794c9
Caladen Temmo <caladentemmo@Caladens-MacBook-Pro.local>
2020-04-21 11:58:03 +1000

    intro

---
+++ start_game.rb
@@ -0,0 +1,17 @@
+class Start_game
+
+puts "Hello noble knight what is thy name".each_char { |char| 
+    putc char     
+    $stdout.flush
+    sleep 0.10
+}
+  
+    end
+    #  name = gets.strip.capitalize
+    #  puts "Hello Sir #{name}"
+    #  puts "*Hands you scroll which says* hello dost thou accept the quest to save my daughter the princess".each_char do |char|
+    #     putc char
+    #     $stdout.flush
+    #     sleep 0.01
+    # end
+
